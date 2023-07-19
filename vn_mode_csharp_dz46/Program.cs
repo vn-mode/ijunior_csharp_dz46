0d8674347f83a0333558d63a4ad0b3a1d5d0ca6b
VITALII NAGIBIN <98809212+vn-mode@users.noreply.github.com>
2023-07-19 19:04:04 +0300

    ijunior_scharp_dz46

--- vn_mode_csharp_dz46/Program.cs
+++ vn_mode_csharp_dz46/Program.cs
@@ -7,21 +7,21 @@ class Program
     static void Main(string[] args)
     {
         Supermarket supermarket = new Supermarket();
-        supermarket.Products.Add(new Product { Name = "Яблоко", Price = 1.2m });
-        supermarket.Products.Add(new Product { Name = "Апельсин", Price = 1.1m });
-        supermarket.Products.Add(new Product { Name = "Банан", Price = 0.9m });
+        supermarket.AddProduct("Яблоко", 1.2m);
+        supermarket.AddProduct("Апельсин", 1.1m);
+        supermarket.AddProduct("Банан", 0.9m);
 
         Customer customer1 = new Customer { Money = 5 };
-        customer1.AddProductToBasket(supermarket.Products[0]);
-        customer1.AddProductToBasket(supermarket.Products[2]);
+        customer1.AddProductToBasket(supermarket.GetProduct(0));
+        customer1.AddProductToBasket(supermarket.GetProduct(2));
         Console.WriteLine("Покупатель 1 добавил в корзину Яблоко и Банан.");
 
         supermarket.AddCustomerToQueue(customer1);
         Console.WriteLine("Покупатель 1 встал в очередь.");
 
         Customer customer2 = new Customer { Money = 3 };
-        customer2.AddProductToBasket(supermarket.Products[1]);
-        customer2.AddProductToBasket(supermarket.Products[2]);
+        customer2.AddProductToBasket(supermarket.GetProduct(1));
+        customer2.AddProductToBasket(supermarket.GetProduct(2));
         Console.WriteLine("Покупатель 2 добавил в корзину Апельсин и Банан.");
 
         supermarket.AddCustomerToQueue(customer2);
@@ -34,13 +34,19 @@ class Program
 
 public class Product
 {
-    public string Name { get; set; }
-    public decimal Price { get; set; }
+    public string Name { get; private set; }
+    public decimal Price { get; private set; }
+
+    public Product(string name, decimal price)
+    {
+        Name = name;
+        Price = price;
+    }
 }
 
 public class Customer
 {
-    public List<Product> Basket { get; set; } = new List<Product>();
+    private List<Product> Basket { get; } = new List<Product>();
     public decimal Money { get; set; }
 
     public decimal BasketTotal
@@ -53,39 +59,50 @@ public class Customer
         Basket.Add(product);
     }
 
-    public bool Pay()
+    public void Pay()
     {
-        if (BasketTotal <= Money)
+        while (!CanPay())
         {
-            Console.WriteLine($"Покупатель оплатил {BasketTotal:C2} и уходит с {(Money - BasketTotal):C2} в кармане.");
-            Money -= BasketTotal;
-            Basket.Clear();
-            return true;
+            RemoveRandomProduct();
         }
 
-        while (Basket.Count > 0 && BasketTotal > Money)
-        {
-            var removedProduct = Basket.ElementAt(new Random().Next(Basket.Count));
-            Console.WriteLine($"Покупатель не может оплатить все товары. Удаляем {removedProduct.Name} из корзины.");
-            Basket.Remove(removedProduct);
-        }
+        PerformPayment();
+    }
 
-        if (BasketTotal <= Money)
-        {
-            Console.WriteLine($"Покупатель оплатил {BasketTotal:C2} и уходит с {(Money - BasketTotal):C2} в кармане.");
-            Money -= BasketTotal;
-            Basket.Clear();
-            return true;
-        }
+    private bool CanPay()
+    {
+        return BasketTotal <= Money;
+    }
 
-        return false;
+    private void RemoveRandomProduct()
+    {
+        var removedProduct = Basket.ElementAt(new Random().Next(Basket.Count));
+        Console.WriteLine($"Покупатель не может оплатить все товары. Удаляем {removedProduct.Name} из корзины.");
+        Basket.Remove(removedProduct);
+    }
+
+    private void PerformPayment()
+    {
+        Console.WriteLine($"Покупатель оплатил {BasketTotal:C2} и уходит с {(Money - BasketTotal):C2} в кармане.");
+        Money -= BasketTotal;
+        Basket.Clear();
     }
 }
 
 public class Supermarket
 {
-    public List<Product> Products { get; set; } = new List<Product>();
-    public List<Customer> Queue { get; set; } = new List<Customer>();
+    private List<Product> Products { get; } = new List<Product>();
+    private List<Customer> Queue { get; } = new List<Customer>();
+
+    public void AddProduct(string name, decimal price)
+    {
+        Products.Add(new Product(name, price));
+    }
+
+    public Product GetProduct(int index)
+    {
+        return Products[index];
+    }
 
     public void AddCustomerToQueue(Customer customer)
     {
