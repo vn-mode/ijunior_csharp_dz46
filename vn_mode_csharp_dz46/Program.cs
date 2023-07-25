158af17bc858dc55d960220e719895b842c9e368
VITALII NAGIBIN <98809212+vn-mode@users.noreply.github.com>
2023-07-25 19:53:59 +0300

    ijunior_csharp_dz46

--- vn_mode_csharp_dz46/Program.cs
+++ vn_mode_csharp_dz46/Program.cs
@@ -11,24 +11,19 @@ class Program
         supermarket.AddProduct("Апельсин", 1.1m);
         supermarket.AddProduct("Банан", 0.9m);
 
-        Customer customer1 = new Customer { Money = 5 };
+        Customer customer1 = new Customer(5);
         customer1.AddProductToBasket(supermarket.GetProduct(0));
         customer1.AddProductToBasket(supermarket.GetProduct(2));
-        Console.WriteLine("Покупатель 1 добавил в корзину Яблоко и Банан.");
 
         supermarket.AddCustomerToQueue(customer1);
-        Console.WriteLine("Покупатель 1 встал в очередь.");
 
-        Customer customer2 = new Customer { Money = 3 };
+        Customer customer2 = new Customer(3);
         customer2.AddProductToBasket(supermarket.GetProduct(1));
         customer2.AddProductToBasket(supermarket.GetProduct(2));
-        Console.WriteLine("Покупатель 2 добавил в корзину Апельсин и Банан.");
 
         supermarket.AddCustomerToQueue(customer2);
-        Console.WriteLine("Покупатель 2 встал в очередь.");
 
         supermarket.ProcessCustomers();
-        Console.WriteLine("Все покупатели в очереди были обслужены.");
     }
 }
 
@@ -46,17 +41,23 @@ public class Product
 
 public class Customer
 {
-    private List<Product> Basket { get; } = new List<Product>();
-    public decimal Money { get; set; }
+    private List<Product> basket = new List<Product>();
+    public decimal Money { get; private set; }
+
+    public Customer(decimal money)
+    {
+        Money = money;
+    }
 
     public decimal BasketTotal
     {
-        get { return Basket.Sum(product => product.Price); }
+        get { return basket.Sum(product => product.Price); }
     }
 
     public void AddProductToBasket(Product product)
     {
-        Basket.Add(product);
+        basket.Add(product);
+        Console.WriteLine($"Покупатель добавил в корзину {product.Name}.");
     }
 
     public void Pay()
@@ -76,45 +77,48 @@ public class Customer
 
     private void RemoveRandomProduct()
     {
-        var removedProduct = Basket.ElementAt(new Random().Next(Basket.Count));
+        var removedProduct = basket.ElementAt(new Random().Next(basket.Count));
         Console.WriteLine($"Покупатель не может оплатить все товары. Удаляем {removedProduct.Name} из корзины.");
-        Basket.Remove(removedProduct);
+        basket.Remove(removedProduct);
     }
 
     private void PerformPayment()
     {
         Console.WriteLine($"Покупатель оплатил {BasketTotal:C2} и уходит с {(Money - BasketTotal):C2} в кармане.");
         Money -= BasketTotal;
-        Basket.Clear();
+        basket.Clear();
     }
 }
 
 public class Supermarket
 {
-    private List<Product> Products { get; } = new List<Product>();
-    private List<Customer> Queue { get; } = new List<Customer>();
+    private List<Product> products = new List<Product>();
+    private List<Customer> queue = new List<Customer>();
 
     public void AddProduct(string name, decimal price)
     {
-        Products.Add(new Product(name, price));
+        products.Add(new Product(name, price));
     }
 
     public Product GetProduct(int index)
     {
-        return Products[index];
+        return products[index];
     }
 
     public void AddCustomerToQueue(Customer customer)
     {
-        Queue.Add(customer);
+        queue.Add(customer);
+        Console.WriteLine($"Покупатель встал в очередь. У него {customer.BasketTotal:C2} товаров на {customer.Money:C2}.");
     }
 
     public void ProcessCustomers()
     {
-        foreach (var customer in Queue)
+        foreach (var customer in queue)
         {
             customer.Pay();
         }
-        Queue.Clear();
+
+        Console.WriteLine("Все покупатели в очереди были обслужены.");
+        queue.Clear();
     }
 }
